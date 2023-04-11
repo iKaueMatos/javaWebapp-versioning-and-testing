bf0850797d4fca00fdf5f330af7736b6d379edb5
IkaueMatos <kaue178hotimail@gmail.com>
2023-04-11 19:33:31 -0300

    atualização

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js,css}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
