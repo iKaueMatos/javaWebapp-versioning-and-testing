74b2ea0c153c53dba32866e9dd7eb078568bf6b0
Pedro <pedro.phprado@gmail.com>
2023-04-15 22:03:42 -0300

    v1.0.0

---
+++ src/main/java/model/Caracteristicas.java
@@ -0,0 +1,15 @@
+package model;
+
+public abstract class Caracteristicas {
+	
+	/* Atributos */
+	
+	private String nome;
+	private float preco;
+	private float peso;
+	private float valorEnergetico;
+	private String regiaoIdeal;
+	
+	
+
+}
---
+++ src/main/java/model/dados.java
@@ -0,0 +1,80 @@
+package model;
+
+public class dados {
+	private int id;
+    private String texto1;
+    private int texto2;
+    private double texto3;
+
+
+    //Construtor padrão 
+    public dados() {
+
+    }
+
+
+
+    //2 Construtor
+    public dados(int id,String texto1,int texto2,double texto3) {
+        this.id = id;
+        this.texto1 = texto1;
+        this.texto2 = texto2;
+        this.texto3 = texto3;
+    }
+
+
+    //3 Construtor
+        public dados(String texto1,int texto2,double texto3) {
+            this.texto1 = texto1;
+            this.texto2 = texto2;
+            this.texto3 = texto3;
+        }
+
+
+        //Getters e Setters
+        public int getId() {
+            return id;
+        }
+
+
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+
+
+        public String getTexto1() {
+            return texto1;
+        }
+
+
+
+        public void setTexto1(String texto1) {
+            this.texto1 = texto1;
+        }
+
+
+
+        public int getTexto2() {
+            return texto2;
+        }
+
+
+
+        public void setTexto2(int texto2) {
+            this.texto2 = texto2;
+        }
+
+
+
+        public double getTexto3() {
+            return texto3;
+        }
+
+
+
+        public void setTexto3(double texto3) {
+            this.texto3 = texto3;
+        }
+}
