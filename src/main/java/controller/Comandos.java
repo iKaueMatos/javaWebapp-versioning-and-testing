fce93aea6083147c3c21fc390fe0831422d9b3b9
Pedro <pedro.phprado@gmail.com>
2023-04-20 23:16:41 -0300

    Iniciando a validacao

--- src/main/java/controller/Comandos.java
+++ src/main/java/controller/Comandos.java
@@ -1,5 +1,31 @@
 package controller;
 
 public class Comandos {
+	
+	//Método validação string
+	
+	public static boolean validaString(String entrada) {
+		entrada = entrada.toLowerCase();
+		entrada = entrada.replaceAll("\\s+", ""); //Retira os espaços da entrada
+		boolean resultado = entrada.matches("[a-z]{3,}");
+		System.out.println(entrada);
+		return resultado;
+	}
+	
+	
+	//Método validação numerico
+	
+	public static boolean validaNumerico(String numerico){
 
+		String n1 = "690";
+		
+		numerico = numerico.replaceAll("\\s+", "");
+		boolean resultado = numerico.matches("[0-9]{1,10}");
+		
+		
+		return resultado;
+	} 
+	
+	
+	
 }
--- src/main/java/controller/cadastrarDados.java
+++ src/main/java/controller/cadastrarDados.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import controller.Comandos;
 
 	/**	
 	  
@@ -51,7 +52,8 @@ import javax.servlet.http.HttpServletResponse;
 	        System.out.println("Texto 3" + texto3);
 
 	        //Lógica da Comparação
-	        
+	        System.out.println(Comandos.validaString(texto1));
+	        System.out.println(Comandos.validaNumerico(texto2));
 
 	    }
 	}
