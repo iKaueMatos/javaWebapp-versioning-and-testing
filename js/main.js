e7e0a4e0c476e021ef7cc6c2b81f20888088e254
IkaueMatos <kaue178hotimail@gmail.com>
2023-04-28 19:42:34 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // mobile bar
 
 const menu = document.getElementById("mobile-bar")
-console.log(menu);
 menu.addEventListener("click", () => {
     document.getElementById("menu").classList.toggle("active")
 })
